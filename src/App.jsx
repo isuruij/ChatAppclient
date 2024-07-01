e44d47d4d56be1737065881872994bf0111a11cb
Isuru Jayalath <2020uelog@gmail.com>
2024-07-01 11:23:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io.connect("http://ec2-13-60-34-197.eu-north-1.compute.amazonaws.com:3000");
+const socket = io.connect("13.51.201.88:3000");
 
 function App() {
   const [username, setUsername] = useState("");
